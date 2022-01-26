bff0968ff59c9dd1fe78a306703ec7d230ca975d
Matteo <matteo@swappie.com>
2022-01-26 20:20:40 +0200

    Updates

---
+++ precache-manifest.326fa41bbbd9c47a92f7bd7198730703.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe8164d6b8b23c5671757c997ece2873",
+    "url": "/ackorchmaros.github.io/index.html"
+  },
+  {
+    "revision": "5ded3c727d605d40479c",
+    "url": "/ackorchmaros.github.io/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "ce571bc2cca99456e5d0",
+    "url": "/ackorchmaros.github.io/static/js/2.82d857d4.chunk.js"
+  },
+  {
+    "revision": "5ded3c727d605d40479c",
+    "url": "/ackorchmaros.github.io/static/js/main.965bbbb3.chunk.js"
+  },
+  {
+    "revision": "eafd92617c3a80aecabf",
+    "url": "/ackorchmaros.github.io/static/js/runtime~main.d66106af.js"
+  }
+]);

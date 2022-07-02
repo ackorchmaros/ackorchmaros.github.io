e80f11115335bf64b9d03b2a345b19c13d25c045
Matteo <matteo@swappie.com>
2022-07-02 20:44:43 +0300

    Updates

---
+++ precache-manifest.d04c6ac539f9c54c85e4534211572612.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b389949fe0bb39e24568387f0178a035",
+    "url": "/index.html"
+  },
+  {
+    "revision": "896d79436cd2c3d9a879",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "ce571bc2cca99456e5d0",
+    "url": "/static/js/2.82d857d4.chunk.js"
+  },
+  {
+    "revision": "896d79436cd2c3d9a879",
+    "url": "/static/js/main.91e4b2dc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
